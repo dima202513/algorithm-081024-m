0356bb422263b0a94675990c8929fa8a09754fe8
dima202513 <dmitrijra@gmail.com>
2025-02-24 11:25:59 +0100

    Update Hanoi.java
     if (n == 0) return;
    pri proverke gpt dopisal  k reshenijy
    nesovsem ponyal, no reshenie vidalo

--- src/main/java/org/telran/lecture_03_recursion/home_work/Hanoi.java
+++ src/main/java/org/telran/lecture_03_recursion/home_work/Hanoi.java
@@ -1,17 +1,17 @@
 package org.telran.lecture_03_recursion.home_work;
-
 public class Hanoi {
-
+    // n: количество дисков
+    // start: начальный столбец
+    // end: конечный столбец
+    // aux: вспомогательный столбец
     public static void hanoi(int n, String start, String end, String aux) {
-        // n: количество дисков
-        // start: начальный столбец
-        // end: конечный столбец
-        // aux: вспомогательный столбец
-
-        // You code...
+        if (n == 0) return;
+        hanoi(n - 1, start, aux, end);
+        System.out.println(start + " go " + end);
+        hanoi(n - 1, aux, end, start);
     }
 
     public static void main(String[] args) {
-        hanoi(3, "A", "C", "B"); // Выводит в терминал описание действие по перемещению дисков "A" --> "C"
+        hanoi(5, "A", "C", "B");
     }
 }
